7748eef2a982b902d9ba870c2fee69cc3ee45c3a
Jose Matheus Araujo Bessa <joseb@drconsulta.com>
2021-03-19 16:53:09 -0300

    Implement async await

--- index.js
+++ index.js
@@ -5,35 +5,39 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const artifact = require('@actions/artifact');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const sha = core.getInput('sha');
-  const repository = core.getInput('repository');
-  const accessToken = core.getInput('access-token');
-  const repositoryOwner = core.getInput('repo-owner');
-  const reportArtifactName = core.getInput('report-artifact-name');
-
-
-  console.log(`Hello ${sha}!`);
-  console.log(`Hello ${repository}!`);
-  console.log(`Hello ${accessToken}!`);
-  console.log(`Hello ${repositoryOwner}!`);
-  console.log(`Hello ${reportArtifactName}!`);
-
-  const downloadResponse = await artifactClient.downloadArtifact(reportArtifactName, '/reports', { createArtifactFolder: false });
-  console.log('Download: ', downloadResponse);
-
-  fs.readFile( `${downloadResponse.downloadPath}/output.xml`, function(err, data) {
-    // var json = JSON.parse(parser.toJson(data, {reversible: true}));
-    console.log('reading report xml: ' + data);
-    // console.log('reading report json: ' + json);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+async function init() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const sha = core.getInput('sha');
+    const repository = core.getInput('repository');
+    const accessToken = core.getInput('access-token');
+    const repositoryOwner = core.getInput('repo-owner');
+    const reportArtifactName = core.getInput('report-artifact-name');
+  
+  
+    console.log(`Hello ${sha}!`);
+    console.log(`Hello ${repository}!`);
+    console.log(`Hello ${accessToken}!`);
+    console.log(`Hello ${repositoryOwner}!`);
+    console.log(`Hello ${reportArtifactName}!`);
+  
+    const downloadResponse = await artifactClient.downloadArtifact(reportArtifactName, '/reports', { createArtifactFolder: false });
+    console.log('Download: ', downloadResponse);
+  
+    fs.readFile( `${downloadResponse.downloadPath}/output.xml`, function(err, data) {
+      // var json = JSON.parse(parser.toJson(data, {reversible: true}));
+      console.log('reading report xml: ' + data);
+      // console.log('reading report json: ' + json);
+      const time = (new Date()).toTimeString();
+      core.setOutput("time", time);
+      
+    });
     
-  });
-  
-  
-  
-} catch (error) {
-  core.setFailed(error.message);
+    
+    
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+init();
