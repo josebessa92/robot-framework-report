e8f3ed8455ba972d215e1edbe654a51086b874be
Jose Matheus Araujo Bessa <joseb@drconsulta.com>
2021-03-19 12:58:34 -0300

    Inclusão de novos inputs para o action

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
+
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const sha = core.getInput('sha');
+  const repository = core.getInput('repository');
+  const reportPath = core.getInput('report-path');
+  const accessToken = core.getInput('access-token');
+  const repositoryOwner = core.getInput('repo-owner');
+
+
+  console.log(`Hello ${sha}!`);
+  console.log(`Hello ${repository}!`);
+  console.log(`Hello ${reportPath}!`);
+  console.log(`Hello ${accessToken}!`);
+  console.log(`Hello ${repositoryOwner}!`);
+
+
+  fs.readFile( `${reportPath}/output.xml`, function(err, data) {
+    var json = JSON.parse(parser.toJson(data, {reversible: true}));
+    console.log('reading report: ' + json);
+  });
+  
+  
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  
+  
 } catch (error) {
   core.setFailed(error.message);
 }
